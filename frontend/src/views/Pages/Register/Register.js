1adf22ec0406bc966bece5ec5985f3e05dd9ca89
Cleverson Faria <cleversonffaria@gmail.com>
2019-09-26 09:11:04 -0300

    Starting this projet

---
+++ frontend/src/views/Pages/Register/Register.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <Form>
+                    <h1>Registro</h1>
+                    <p className="text-muted">Criar sua conta</p>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Nome" autoComplete="username" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>@</InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Email" autoComplete="email" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Senha" autoComplete="new-password" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Repetir senha" autoComplete="new-password" />
+                    </InputGroup>
+                    <Button color="success" block>Criar Conta</Button>
+                  </Form>
+                </CardBody>
+                <CardFooter className="p-4">
+                  <Row>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-facebook mb-1" block><span>Facebook</span></Button>
+                    </Col>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-twitter mb-1" block><span>Twitter</span></Button>
+                    </Col>
+                  </Row>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Register;
