2a5b8005a879f25eef2e2d1208869ea9ba2323c5
Cleverson Faria <cleversonffaria@gmail.com>
2019-10-12 11:12:23 -0300

    Controller modificado

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -1,16 +1,20 @@
 "use strict";
 const User = use("App/Models/User");
 const Database = use("Database");
+const { validate } = use("Validator");
+function toLower(word) {
+  if (word) {
+    return word.toLowerCase();
+  }
+}
 class UserController {
   async index({ auth, response }) {
     const user = auth.getUser();
     if (user) {
-      return response
-        .status(200)
-        .send({
-          username: auth.user.username,
-          user_status: auth.user.user_status
-        });
+      return response.status(200).send({
+        username: auth.user.username,
+        user_status: auth.user.user_status
+      });
     }
     response
       .status(401)
@@ -18,8 +22,46 @@ class UserController {
   }
   async create({ request, response }) {
     try {
-      const data = request.only(["username", "email", "password"]);
-      const user = await User.create({ ...data, user_status: 3 });
+      const { username: name, email: mail, password } = request.only([
+        "username",
+        "email",
+        "password"
+      ]);
+
+      const username = toLower(name);
+      const email = toLower(mail);
+
+      const rules = {
+        username: `required|min:5|unique:users`,
+        email: "required|email|unique:users",
+        password: "required|min:6"
+      };
+      const messages = {
+        "username.required": "Esse campo é obrigatório",
+        "username.unique": "Esse usuário já existe",
+        "username.min": "O usuário deve ter mais que 5 caracteres",
+        "email.required": "Email é obrigatório",
+        "email.unique": "Esse email já existe",
+        "email.email": "Esse email não é valido",
+        "password.required": "Senha não informada",
+        "password.min": "A senha deve ter mais que 6 caracteres"
+      };
+      const validation = await validate(
+        { username, email, password },
+        rules,
+        messages
+      );
+
+      if (validation.fails()) {
+        return response.status(202).send(validation.messages());        
+      }
+
+      const user = await User.create({
+        password,
+        email,
+        username,
+        user_status: 3
+      });
       return user;
     } catch (error) {
       return response.status(500).send({
@@ -60,7 +102,12 @@ class UserController {
   }
   async updateUser({ request, params, response, auth }) {
     try {
-      const { username, email, password, user_status } = request.all();
+      const {
+        username: name,
+        email: mail,
+        password,
+        user_status
+      } = request.all();
       const user = await User.findBy("id", params.id);
       if (!user) {
         return response
@@ -69,6 +116,30 @@ class UserController {
       }
       if (user.id != auth.user.id && auth.user.user_status === 1) {
         user.user_status = user_status;
+      }
+      const username = toLower(name);
+      const email = toLower(mail);
+
+      const rules = {
+        username: `min:5|unique:users`,
+        email: "email|unique:users",
+        password: "min:6"
+      };
+      const messages = {
+        "username.unique": "Esse usuário já existe",
+        "username.min": "O usuário deve ter mais que 5 caracteres",
+        "email.unique": "Esse email já existe",
+        "email.email": "Esse email não é valido",
+        "password.min": "A senha deve ter mais que 6 caracteres"
+      };
+      const validation = await validate(
+        { username, email, password },
+        rules,
+        messages
+      );
+
+      if (validation.fails()) {
+        return response.status(202).send(validation.messages());
       }
       if (params.id == auth.user.id || auth.user.user_status === 1) {
         user.password = password;
